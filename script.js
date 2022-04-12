8601c52fe77f4ad32baecd3cd60074478a5b575d
Phillip Pena <ppena941@gmail.com>
2022-04-12 18:59:05 -0400

    - initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//utility functions
+function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+function qsa(selector, parent = document) {
+  return parent.querySelectorAll(selector);
+}
