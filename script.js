6bb3f05f051f0ff03bc5f6c294f3060f689bf22e
Phillip Pena <ppena941@gmail.com>
2022-04-12 20:29:45 -0400

    - etch-a-sketch working, next toolbar

--- script.js
+++ script.js
@@ -4,5 +4,95 @@ function qs(selector, parent = document) {
 }
 
 function qsa(selector, parent = document) {
-  return parent.querySelectorAll(selector);
+  return [...parent.querySelectorAll(selector)];
 }
+
+function addGlobalEventListener(
+  type,
+  selector,
+  callback,
+  options,
+  parent = document
+) {
+  parent.addEventListener(
+    type,
+    e => {
+      if (e.target.matches(selector)) callback(e);
+    },
+    options
+  );
+}
+
+function createElement(type, options = {}) {
+  const element = document.createElement(type);
+  Object.entries(options).forEach(([key, value]) => {
+    if (key === "class") {
+      element.classList.add(value);
+      return;
+    }
+
+    if (key === "dataset") {
+      Object.entries(value).forEach(([dataKey, dataValue]) => {
+        element.dataset[dataKey] = dataValue;
+      });
+      return;
+    }
+
+    if (key === "text") {
+      element.textContent = value;
+      return;
+    }
+
+    element.setAttribute(key, value);
+  });
+  return element;
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+//DOM elements
+let slider = qs("#slider");
+let sketchArea = qs("#sketch-area");
+let colorPicker = qs("#color-picker");
+let rainbowBtn = qs("#rainbow");
+let greyscaleBtn = qs("#greyscale");
+let eraserBtn = qs("#eraser");
+let clearBtn = qs("#clear");
+
+//variable declarations
+let gridLength = 32;
+
+//app functions
+function drawGrid(dimension) {
+  sketchArea.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+  let gridArea = dimension * dimension;
+
+  if (sketchArea.firstChild) {
+    removeAllChildNodes(sketchArea);
+  }
+  for (let i = 0; i < gridArea; i++) {
+    let item = createElement("div", { class: "grid-item" });
+    sketchArea.appendChild(item);
+  }
+}
+
+function paint(e) {
+  e.target.style.backgroundColor = colorPicker.value;
+}
+
+//window on load
+drawGrid(gridLength);
+
+//event listeners
+addGlobalEventListener("click", ".tool", () => console.log("tool clicked"));
+addGlobalEventListener("mouseover", ".grid-item", e => paint(e));
+
+slider.addEventListener("change", e => {
+  gridLength = e.target.value;
+  drawGrid(gridLength);
+  console.log(gridLength);
+});
